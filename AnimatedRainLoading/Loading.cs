050ba8dff663ad72c31d2a43b7d2e1f4b64bcaf4
Gabriel Poppes <gabrielpoppes@outlook.com>
2021-10-07 08:50:28 -0300

    Adicionar arquivos de projeto.

---
+++ AnimatedRainLoading/Loading.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnimatedRainLoading
+{
+    public partial class Loading : Form
+    {
+        public Loading()
+        {
+            InitializeComponent();
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 7, 7)); // Arredondando as bordas
+        }
+    }
+}
